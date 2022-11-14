23b4ca808bd2aa2e8674d2d4757ea53d8acff6cb
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-11-15 00:27:24 +0100

    Calculate severity based on severity_label

--- frontend/src/pages/audits/edit/index.vue
+++ frontend/src/pages/audits/edit/index.vue
@@ -349,7 +349,46 @@ export default {
 				}
 			},
 
+			getFindingSeverityCustom: function(finding){
+				try {
+					let locale = this.audit.language; // does "this" hold for all places?
+
+					let severity_label_field = finding.customFields.filter((el) => el.customField.label === 'severity_label')[0];
+
+					let text_severity = severity_label_field.text;
+					let all_options = severity_label_field.customField.options;
+					
+					let severity_index = all_options.filter((el) => el.locale === locale).map(x => x.value).indexOf(text_severity);
+					let unified_severity_level = this.unifiedSeverityLevels()[severity_index];
+
+					return unified_severity_level;
+				} catch (e){
+					return undefined;
+				}
+			},
+
+			unifiedSeverityLevels: function(){
+				return ['none', 'low', 'medium', 'high', 'critical'];
+			},
+			
+			unifiedSeverityToEquivalentCVSSString: function(unified_severity_string){
+				let levels = {
+					critical: 	"CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:C/C:H/I:H/A:H",
+					high: 		"CVSS:3.1/AV:P/AC:L/PR:N/UI:N/S:C/C:H/I:H/A:H",
+					medium: 	"CVSS:3.1/AV:P/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
+					low: 		"CVSS:3.1/AV:P/AC:H/PR:H/UI:R/S:U/C:N/I:L/A:N",
+					none: 		"CVSS:3.1/AV:P/AC:L/PR:N/UI:N/S:U/C:N/I:N/A:N",
+				};
+				return levels[unified_severity_string];
+			},
+
 			getFindingSeverity: function(finding) {
+				let customSeverity = this.getFindingSeverityCustom(finding);
+				if (customSeverity !== undefined) {
+					let equivalent_cvss = this.unifiedSeverityToEquivalentCVSSString(customSeverity);
+					finding.cvssv3 = equivalent_cvss;
+				}
+
 				let severity = "None"
 				let cvss = CVSS31.calculateCVSSFromVector(finding.cvssv3)
 				if (cvss.success) {
@@ -734,6 +773,6 @@ export default {
 
 <style>
 .tns-severity-icon {
-	display: none;
+	/* display: none; */
 }
 </style>
